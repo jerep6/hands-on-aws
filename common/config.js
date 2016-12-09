a895b48e1d13895f645d1cba7e82759a079f16d6
Jérémy Pinsolle <jpinsolle@xebia.fr>
2016-12-09 15:02:47 +0100

    Lambda environment variable "GroupName"

--- common/config.js
+++ common/config.js
@@ -1,11 +1,11 @@
 const TP_GROUP_NAME = "group1";
 
 module.exports = {
-  groupName: TP_GROUP_NAME,
+  groupName: process.env.GroupName || TP_GROUP_NAME,
   logmatic: {
     host: "api.logmatic.io",
     key:  "vnfON-PWTc-Ne0GS_JQsKg",
-    name: `hands-on-${TP_GROUP_NAME}`,
+    name: `hands-on-${process.env.GroupName || TP_GROUP_NAME}`,
     port: 10515,
     use: true
   },
